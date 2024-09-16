81668b58d3d91b70c02014bc73398550034dd64d
Grigorasch <pgv856@gmail.com>
2024-09-16 07:11:23 +0600

    refactor: output test

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import plugin from '../plugin.json';
 import * as acorn from "acorn";
 const fs = acode.require('fs');
+const { Range } = ace.require("ace/range");
+const Url = acode.require('Url');
 
 class Tree {
   constructor(pathToFile) {
     this.pathToFile = pathToFile;
+
     const readFilePromise = this._readFile(this.pathToFile);
     readFilePromise.then(code => {
       this.entities = this._buildTree(code);
-      console.log(this.entities)
+      console.log(this)
     })
         .catch(console.error);
   }
@@ -20,28 +23,43 @@ class Tree {
   }
 
   _buildTree(code) {
-    const entities = {};
+    const entities = {
+      variables: [],
+    };
     // TODO FIX Добавить автоматический подбор версии ecma на основе настроек в packagee=.json и прочих
-    const ast = acorn.parse(code, {ecmaVersion: 2020, sourceType: "module"});
+    // TODO FIX Добавить автоматический подбор версии типа (модуль или скрипт) на основе настроек в packagee=.json и прочих
+    const ast = acorn.parse(code, {ecmaVersion: 2020, sourceType: "module", locations: true,});
     ast.body.forEach(node => {
-      if (node.type === 'VariableDeclaration') {
-        node.declarations.forEach(declaration => {
-          entities[declaration.id.name] = {
-            type: 'variable',
-            parent: null
-          };
-        });
-      } else if (node.type === 'FunctionDeclaration') {
-        entities[node.id.name] = {
-          type: 'function',
-          parent: null
-        };
-      } else if (node.type === 'ClassDeclaration') {
-        entities[node.id.name] = {
-          type: 'class',
-          parent: null
-        }
+      switch (node.type) {
+        case "VariableDeclaration":
+          const varRange = new Range(node.loc.start.line, node.loc.start.column, node.loc.end.line, node.loc.end.column)
+          const variable = {
+            location: varRange,
+            text: editorManager.getSession().getTextRange(varRange),
+            name: node.declarations.map(decloration => decloration.id.name)
+          }
+          entities.variables.push(variable);
+          break;
       }
+      //
+      // if (node.type === 'VariableDeclaration') {
+      //   node.declarations.forEach(declaration => {
+      //     entities[declaration.id.name] = {
+      //       type: 'variable',
+      //       parent: null
+      //     };
+      //   });
+      // } else if (node.type === 'FunctionDeclaration') {
+      //   entities[node.id.name] = {
+      //     type: 'function',
+      //     parent: null
+      //   };
+      // } else if (node.type === 'ClassDeclaration') {
+      //   entities[node.id.name] = {
+      //     type: 'class',
+      //     parent: null
+      //   }
+      // }
     });
     return entities;
   }
@@ -51,7 +69,8 @@ class AcodePlugin {
 
   async init() {
     editorManager.on('switch-file', () => {
-      const codeTree = new Tree(editorManager.activeFile.uri);
+      if (Uri.extname(editorManager.activeFile.name) !== '.js') return;
+      const codeTree = new Tree(editorManager.activeFile.cacheFile);
     })
   }
 
